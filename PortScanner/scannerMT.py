1af09d9492eccdd766db1a112de104bd3a54e854
pmacedo-19 <wardian@protonmail.com>
2023-10-30 22:59:31 +0000

    Multithreaded port scanner

---
+++ PortScanner/scannerMT.py
@@ -0,0 +1,44 @@
+import socket
+import threading
+import time
+
+# Function to scan a specific port on the target
+def scan_port(target, port):
+    try:
+        # Create a socket
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # Set a timeout for the connection attempt (1 second)
+        sock.settimeout(1)
+        # Attempt to connect to the target and port
+        result = sock.connect_ex((target, port))
+        if result == 0:
+            # If the connection attempt is successful, the port is open
+            print(f'Port {port}: OPEN')
+        sock.close()
+    except Exception as e:
+        # Handle any exceptions that might occur during the connection attempt
+        pass
+
+# Main function for port scanning
+def main():
+    target = input('Enter host for scanning: ')
+    target_IP = socket.gethostbyname(target)
+    print('Starting scan on host: ', target_IP)
+    
+    threads = []  # List to hold our threads
+
+    for port in range(50, 500):
+        # Create a thread for scanning each port
+        thread = threading.Thread(target=scan_port, args=(target_IP, port))
+        threads.append(thread)
+        thread.start()
+
+    # Wait for all threads to finish
+    for thread in threads:
+        thread.join()
+
+    print("Time taken: ", time.time() - start_time)
+
+if __name__ == "__main__":
+    start_time = time.time()
+    main()
